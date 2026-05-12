a62d93489847fdf651eb2df30a299939581904e0
Benitto <benitto1153@gmail.com>
2026-05-12 06:48:06 +0530

    upated

--- src/test/java/Pages/EmployeePage.java
+++ src/test/java/Pages/EmployeePage.java
@@ -17,22 +17,17 @@ public class EmployeePage {
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    // 🔹 Menu
     By pimMenu = By.xpath("//span[text()='PIM']");
     By addBtn = By.xpath("//button[normalize-space()='Add']");
 
-    // 🔹 Form
     By firstName = By.name("firstName");
     By lastName  = By.name("lastName");
     By saveBtn   = By.xpath("//button[@type='submit']");
 
-    // 🔹 Loader (IMPORTANT)
     By loader = By.xpath("//div[contains(@class,'oxd-form-loader')]");
 
-    // 🔹 Success page
     By personalDetailsHeader = By.xpath("//h6[normalize-space()='Personal Details']");
 
-    // ================= METHODS =================
 
     public void goToPIM() {
         wait.until(ExpectedConditions.elementToBeClickable(pimMenu)).click();
@@ -53,19 +48,15 @@ public class EmployeePage {
         driver.findElement(lastName).clear();
         driver.findElement(lastName).sendKeys(lName);
 
-        // 🔥 WAIT for loader to disappear (CRITICAL FIX)
         wait.until(ExpectedConditions.invisibilityOfElementLocated(loader));
 
-        // Optional scroll (extra safety)
         ((JavascriptExecutor) driver).executeScript(
                 "arguments[0].scrollIntoView(true);",
                 driver.findElement(saveBtn)
         );
 
-        // Click Save
         wait.until(ExpectedConditions.elementToBeClickable(saveBtn)).click();
 
-        // Wait for success page
         wait.until(ExpectedConditions.visibilityOfElementLocated(personalDetailsHeader));
     }
 
